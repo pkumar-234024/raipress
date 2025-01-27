8c812eb00f7e68280ed473db3c97f4af65dcc331
praveen kumar <86702852+visionaspsupport@users.noreply.github.com>
2025-01-27 12:02:18 +0530

    kss

--- @latest/src/App.jsx
+++ @latest/src/App.jsx
@@ -9,19 +9,18 @@ import "./App.css";
 
 function App() {
   return (
-    <Router basename="/">
+    <Router>
       <div>
         <Navbar />
         <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route exact path="/home" element={<Home />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
           <Route
-            exact
             path="/products/category/:categoryId"
             element={<ProductListing />}
           />
-          <Route exact path="/product/:productId" element={<ProductDetail />} />
-          <Route exact path="/admin" element={<AdminPanel />} />
+          <Route path="/product/:productId" element={<ProductDetail />} />
+          <Route path="/admin" element={<AdminPanel />} />
           {/* Add other routes as needed */}
         </Routes>
         {/* <Footer /> */}
